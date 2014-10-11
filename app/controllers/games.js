9baf033dc15791c2942998ccbbac42f50c19407c
darshanasivakumar <darshana10@gmail.com>
2014-10-11 12:54:21 -0700

    Changes

---
+++ app/controllers/games.js
@@ -0,0 +1,10 @@
+var Games = function () {
+  this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
+
+  this.index = function (req, resp, params) {
+    this.respond({params: params});
+  };
+};
+
+exports.Games = Games;
+
